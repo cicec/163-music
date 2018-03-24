bd1c4a039b160107d73193fd0762bf7dc3757257
clancysong <clancysong@gmail.com>
2018-03-24 15:26:27 +0800

    correct event-hub

--- src/admin/js/event-hub.js
+++ src/admin/js/event-hub.js
@@ -1,11 +1,11 @@
 const eventHub = {
     events: {},
     emit(eventName, data) {
-        Object.keys(this.events).forEach((key) => {
-            this.events[key].forEach((func) => {
+        if (this.events[eventName]) {
+            this.events[eventName].forEach((func) => {
                 func(data)
             })
-        })
+        }
     },
     on(eventName, func) {
         if (!this.events[eventName]) this.events[eventName] = []
